a9a209d5a2e7a2c46100a4a23afaff79b9c3f0ce
peter-zhou <imzhousong@gmail.com>
2016-06-29 16:45:20 +0800

    Update AnnotationBean.java
    修复dubbo:annotation扫描时，若目标类有事务控制，则发布服务不成功的BUG。

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/AnnotationBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/AnnotationBean.java
@@ -24,6 +24,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -134,6 +135,14 @@ public class AnnotationBean extends AbstractConfig implements DisposableBean, Be
         if (! isMatchPackage(bean)) {
             return bean;
         }
+        
+        Class <?> clazzImpl = null;
+	if (AopUtils.isAopProxy(bean) || AopUtils.isCglibProxy(bean)) {
+	    clazzImpl = AopUtils.getTargetClass(bean);
+	} else {
+	    clazzImpl = bean.getClass();
+	}
+        
         Service service = bean.getClass().getAnnotation(Service.class);
         if (service != null) {
             ServiceBean<Object> serviceConfig = new ServiceBean<Object>(service);
